c17c44d24953b4c6460f10dcc802e7e8b02fa4d6
Thao Phan <thao_phan@cs.ucsb.edu>
2020-06-28 15:49:03 -0700

    Initial Commit

---
+++ Health and FitnessTests/Health_and_FitnessTests.swift
@@ -0,0 +1,34 @@
+//
+//  Health_and_FitnessTests.swift
+//  Health and FitnessTests
+//
+//  Created by Thao Phan on 6/28/20.
+//  Copyright © 2020 Thao Phan. All rights reserved.
+//
+
+import XCTest
+@testable import Health_and_Fitness
+
+class Health_and_FitnessTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
